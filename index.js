727abb6a4203517524a6dd0d77d1217fd1de8e99
emamjion <emamjion@gmail.com>
2024-05-23 09:11:54 +0600

    single product api done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -40,7 +40,14 @@ async function run() {
     app.get('/products', async(req, res) => {
         const result = await productsCollection.find().toArray();
         res.send(result);
-    })
+    });
+
+    app.get('/products/:productid', async(req, res) => {
+        const id = req.params.productid;
+        const query = {_id : new ObjectId(id)}
+        const result = await productsCollection.findOne(query);
+        res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
